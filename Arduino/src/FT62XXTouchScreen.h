102fa9fe4eef47efd4a01d313bade51f7e96be3b
richard fontaine <ririfonfon10@gmail.com>
2021-11-06 09:04:34 +0100

    Update FT62XXTouchScreen.h

--- Arduino/src/FT62XXTouchScreen.h
+++ Arduino/src/FT62XXTouchScreen.h
@@ -7,7 +7,7 @@
 
   #define _FT62XXTouchScreen_H_ 1
 
-  #define FT62XX_ADDR 0x38
+  #define FT62XX_ADDR 0x38            //!< I2C address
   #define FT62XX_REG_MODE 0x00        //!< Device mode, either WORKING or FACTORY
   #define FT62XX_REG_CALIBRATE 0x02   //!< Calibrate mode
   #define FT62XX_REG_WORKMODE 0x00    //!< Work mode
@@ -123,10 +123,10 @@
         uint16_t touchX = i2cdat[0x05] & 0x0F;
         touchX <<= 8;
         touchX |= i2cdat[0x06];
-        uint16_t touchID = i2cdat[0x05] >> 4;
       
         #ifdef TOUCHSCREEN_DEBUG
           Serial.println();
+          uint16_t touchID = i2cdat[0x05] >> 4;
           for (uint8_t i = 0; i < touches; i++) {
             Serial.print("ID #");
             Serial.print(touchID);
